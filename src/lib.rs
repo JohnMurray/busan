ab9272dc4b1e94d3c6ce39e28a87a477904be142
John Murray <me@johnmurray.io>
2023-01-01 12:50:34 -0500

    remove sample function

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,4 @@
 pub mod actor;
 pub mod executor;
 pub mod system;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod thread_executor;
