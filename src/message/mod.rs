2c6e60cf478b41e32620c07a6a2bc488b3af73f8
John Murray <me@johnmurray.io>
2023-05-02 11:37:46 -0400

    refactor ToMessage to be typed, add encode proxy method

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -3,17 +3,33 @@ pub mod common_types;
 pub trait Message: prost::Message {
     fn as_any(&self) -> &dyn std::any::Any;
 
+    fn encode_to_vec2(&self) -> Vec<u8>;
+
     #[doc(hidden)]
     fn encoded_len(&self) -> usize {
         prost::Message::encoded_len(self)
     }
+
+    // fn decode_from_vec(bytes: Vec<u8>) -> Result<Self, prost::DecodeError>
+    // where
+    //     Self: Sized,
+    // {
+    //     prost::Message::decode(bytes)
+    // }
 }
 
-pub trait ToMessage {
-    fn to_message(self) -> Box<dyn Message>;
+pub trait ToMessage<M: Message> {
+    fn to_message(self) -> M;
 
     fn is_primitive<L: private::IsLocal>(&self) -> bool {
         false
+    }
+}
+
+/// Impl ToMessage for all types that are already messages.
+impl<M: Message> ToMessage<M> for M {
+    fn to_message(self) -> M {
+        self
     }
 }
 
